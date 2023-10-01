32a282431e8452daea2ca7e46347a5bcdb102c4c
Guilherme Tavares <guilherme.tavares@simlabsdev.onmicrosoft.com>
2023-10-01 13:52:45 -0300

    axios api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import React, { useEffect } from "react";
+import { Client } from "@stomp/stompjs";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    connectRabbitMq();
+  }, []);
+
+  function connectRabbitMq() {
+    const client = new Client({
+      brokerURL: "ws://192.168.15.7:15674/ws",
+      connectHeaders: {
+        login: "guest",
+        passcode: "guest",
+      },
+      debug: function (str) {
+        console.log(str);
+      },
+      reconnectDelay: 1000,
+    });
+
+    client.onConnect = function (frame) {
+      console.log("Connected");
+      const subscription = client.subscribe("/queue/test", function (message) {
+        console.log(message);
+      })
+    }
+    client.activate();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
