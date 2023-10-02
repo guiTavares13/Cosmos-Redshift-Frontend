40199af42e211d70698f5a7bb5752ee6e0efdb13
guilherme.tavares <62484044+guiTavares13@users.noreply.github.com>
2023-10-02 16:43:10 -0300

    Ajuste layout navbar e sidebar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faBell } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavBar() {
     return (
         <nav className="nav">
             <div>
-                <p className="icon">Icone</p>
+                <p className="icon"><FontAwesomeIcon icon={faUser} /></p>
             </div>
             <div className="right-side">
-                <p>Bem vindo</p>
+                <p className="welcome">Bem vindo</p>
                 <span className="separator">|</span>
-                <p>Notify</p>
+                <FontAwesomeIcon icon={faBell} />
             </div>
         </nav>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,24 @@
+// Home.js
+
 import React, { useEffect } from "react";
 import connectRabbitMq from '../services/rabbitmq.js';
 import NavBar from '../components/NavBar/Navbar.js';
 import SideBar from "../components/SideBarNavigator/SideBar.js";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import './home.css';
 
 export default function Home() {
+    // Exemplo de dados para o Combobox (altere conforme necessário)
+    const dataOptions = [
+      { title: 'Dado 1' },
+      { title: 'Dado 2' },
+      { title: 'Dado 3' },
+    ];
+
+    const handleComboboxChange = (selectedValue) => {
+      console.log('Valor selecionado:', selectedValue);
+    };
 
     useEffect(() => {
         connectRabbitMq();
@@ -16,10 +29,11 @@ export default function Home() {
             <NavBar />
             <SideBar />
             <div className="container-fluid">
-                <h1>Home</h1>
+                <h1>Selecione o dado que irá visualizar</h1>
                 <div className="row justify-content-center combo-box">
                     <div className="col-md-3">
-                        Coluna 1
+                        
+                    
                     </div>
                     <div className="col-md-3">
                         coluna 2
