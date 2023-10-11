425bff4076ae6d7124a5f6f41bb4ed0b37ac46b9
guilherme.tavares <62484044+guiTavares13@users.noreply.github.com>
2023-10-11 16:23:39 -0300

    inclusão de sleep antes de conectar na fila

--- src/services/rabbitmq.js
+++ src/services/rabbitmq.js
@@ -1,28 +1,36 @@
 import { Client } from "@stomp/stompjs";
 
-function connectRabbitMq(idQueue) {
+// Função sleep para aguardar um período antes de conectar
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-    console.log("aqui é o ID" + idQueue)
-    const client = new Client({
-        brokerURL: "ws://192.168.15.10:15674/ws",
-        connectHeaders: {
-            login: "guest",
-            passcode: "guest",
-        },
-        debug: function (str) {
-            console.log(str);
-        },
-        reconnectDelay: 1000,
+async function connectRabbitMq(idQueue) {
+  console.log(idQueue);
+  const client = new Client({
+    brokerURL: "ws://192.168.15.10:15674/ws",
+    connectHeaders: {
+      login: "guest",
+      passcode: "guest",
+    },
+    debug: function (str) {
+      console.log(str);
+    },
+    reconnectDelay: 1000,
+  });
+
+  // Adicione o sleep antes de conectar na fila (exemplo: 5 segundos)
+  await sleep(10000);
+
+  client.onConnect = function (frame) {
+    console.log("Connected");
+    const subscription = client.subscribe(`${idQueue}`, function (message) {
+      const receivedData = JSON.parse(message.body);
+      console.log("Received message: ", receivedData);
     });
-
-    client.onConnect = function (frame) {
-        console.log("Connected");
-        const subscription = client.subscribe("/queue/" + idQueue, function (message) {
-            const receivedData = JSON.parse(message.body);
-            console.log("Received message: ", receivedData);
-        })
-    }
-    client.activate();
+  };
+  
+  client.activate();
 }
 
 export default connectRabbitMq;
