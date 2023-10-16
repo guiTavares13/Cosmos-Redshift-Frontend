5df94c5e9e61fac5a3076b982bc6d3f6ece61b48
Guilherme Tavares <guilherme.tavares@simlabsdev.onmicrosoft.com>
2023-10-16 16:13:44 -0300

    uso do SockJS StompJs

--- src/services/rabbitmq.js
+++ src/services/rabbitmq.js
@@ -1,38 +1,80 @@
+// import { Client } from "@stomp/stompjs";
+
+// function sleep(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// async function connectRabbitMq(idQueue) {
+//   console.log(idQueue);
+//   const client = new Client({
+//     brokerURL: "ws://192.168.15.7:15674/ws",
+//     connectHeaders: {
+//       login: "guest",
+//       passcode: "guest",
+//     },
+//     debug: function (str) {
+//       console.log(str);
+//     },
+//     reconnectDelay: 1000,
+//   });
+
+//   //await sleep(10000);
+
+//   client.onConnect = function (frame) {
+//     console.log("Connected");
+//     const subscription = client.subscribe(`/queue/${idQueue}`, function (message) {
+//       const receivedData = JSON.parse(message.body);
+//       console.log("Received message: ", receivedData);
+//     });
+//   };
+
+//   client.onWebSocketError = function (event) {
+//     console.error("WebSocket error:", event);
+//   };
+
+//   client.onDisconnect = function (receipt) {
+//     console.log("Disconnected");
+//   };
+  
+  
+  
+//   client.activate();
+// }
+
+// export default connectRabbitMq;
 
 
-import { Client } from "@stomp/stompjs";
+const StompJs = require('@stomp/stompjs');
+const SockJS = require('sockjs-client');
 
-// Função sleep para aguardar um período antes de conectar
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const queue = '/queue/493d1d59-6bf3-4fbc-985f-ad3fe3877443';
 
 async function connectRabbitMq(idQueue) {
-  console.log(idQueue);
-  const client = new Client({
-    brokerURL: "ws://192.168.15.10:15674/ws",
+
+  const client = new StompJs.Client({
+    webSocketFactory: () => new SockJS('http://192.168.15.7/ws'),
     connectHeaders: {
-      login: "guest",
-      passcode: "guest",
+      login: 'guest',
+      passcode: 'guest'
     },
     debug: function (str) {
-      console.log(str);
+      console.log('STOMP: ' + str);
     },
-    reconnectDelay: 1000,
+    reconnectDelay: 5000,
+    heartbeatIncoming: 4000,
+    heartbeatOutgoing: 4000
   });
 
-  // Adicione o sleep antes de conectar na fila (exemplo: 5 segundos)
-  await sleep(10000);
-
   client.onConnect = function (frame) {
-    console.log("Connected");
-    const subscription = client.subscribe(`/queue/${idQueue}`, function (message) {
-      const receivedData = JSON.parse(message.body);
-      console.log("Received message: ", receivedData);
+    console.log('Connected');
+    
+    client.subscribe(idQueue, function (message) {
+      console.log('Received message: ' + message.body);
     });
   };
-  
+
   client.activate();
+
 }
 
 export default connectRabbitMq;
